9b3f1b818bb4673546bc37e3f058c94ce0864367
NereusOcean <niki_5555@mail.ru>
2021-05-01 18:18:18 +0300

    FFFFFF

--- Jerusalimov/lab3/src/main.cpp
+++ Jerusalimov/lab3/src/main.cpp
@@ -2,6 +2,8 @@
 #include <map>
 #include <vector>
 
+using namespace std;
+
 bool choise = 0;
 
 
@@ -9,22 +11,21 @@ class Graph {
 public:
     void inputGraph() {
         int size;
-        std::cin >> size ;
-        std::cin >> source >> runoff;
-       for(int i =0; i <size;++i) {
+        cin >> size >> source >> runoff;
+        while (size != 0) {
             char u, v;
             int mass;
-            std::cin >> u >> v >> mass;
+            cin >> u >> v >> mass;
             graphK1[u][v] = mass;
-
+            --size;
         }
     }
 
     template<class T>
-    std::vector<std::pair<char, int>> findNeighbor(char sought, T whereSearch) {
+    vector<pair<char, int>> findNeighbor(char sought, T whereSearch) {
 
-        std::vector<std::pair<char, int>> answ;
-        std::pair<char, int> edge;
+        vector<pair<char, int>> answ;
+        pair<char, int> edge;
         for (auto vertex : whereSearch[sought]) {
             edge.first = vertex.first;
             edge.second = vertex.second;
@@ -36,11 +37,11 @@ public:
 
     bool depthSearch() { //поиск в ширину
         if (choise) {
-            std::cout << "\n__________________Breadth First Search___________________\n";
+            cout << "\n__________________Breadth First Search___________________\n";
         }
 
-        std::vector<char> tempVert; //Список вершин которые хотим посетить
-        std::map<char, bool> visitedVertex; // Пара: имя - состояние. Список посещеных вершин
+        vector<char> tempVert; //Список вершин которые хотим посетить
+        map<char, bool> visitedVertex; // Пара: имя - состояние. Список посещеных вершин
         char sought; //искомая вершина. Используется для поиска соседей этой вершины
         visitedVertex[source] = true;
         tempVert.push_back(source);
@@ -49,26 +50,26 @@ public:
         while (!tempVert.empty()) {
 
             if (choise) {
-                std::cout << "\n\tThere are unconsidered vertices in the stack\n";
+                cout << "\n\tThere are unconsidered vertices in the stack\n";
             }
 
             //Смотрим соседей текущей вершины
             sought = tempVert.back();
             tempVert.pop_back();
             if (choise) {
-                std::cout << "\tWe are looking for neighbors, for the vertex: " << sought << "\n\n";
+                cout << "\tWe are looking for neighbors, for the vertex: " << sought << "\n\n";
             }
 
             for (auto enumEdge: findNeighbor(sought, graphK1)) {
 
                 if (choise) {
-                    std::cout << "\t\t\t\t " << sought << "->" << enumEdge.first << " \n";
+                    cout << "\t\t\t\t " << sought << "->" << enumEdge.first << " \n";
                 }
 
                 //Если вес ребра больше нуля и мы не посещали ее
                 if (enumEdge.second > 0 && !visitedVertex[enumEdge.first]) {
                     if (choise) {
-                        std::cout << "\t\t The weight > 0 && not been visited\n"
+                        cout << "\t\t The weight > 0 && not been visited\n"
                              << "\t\tWrite to the path...\n\n";
                     }
                     tempVert.push_back(enumEdge.first); //Добавляем имя вершины для просмотра на след.иттерации
@@ -80,13 +81,13 @@ public:
 
                         if (choise) {
                             char a, b;
-                            std::cout << "\t\tCurrent vertex is equal to source, search is complete!\n \t\tPath is equal: "
+                            cout << "\t\tCurrent vertex is equal to source, search is complete!\n \t\tPath is equal: "
                                  << runoff;
                             for (a = this->runoff; a != this->source; a = b) {
                                 b = path[a];
-                                std::cout << "<-" << path[a];
+                                cout << "<-" << path[a];
                             }
-                            std::cout << "\n\n";
+                            cout << "\n\n";
                         }
 
                         return true;
@@ -96,19 +97,18 @@ public:
             }
         }
         if (choise) {
-            std::cout << "\n\tPATH NOT FOUND!!!\n";
+            cout << "\n\tPATH NOT FOUND!!!\n";
         }
         //Все вершины расмотрены, путь не найден.
         return false;
     }
 
-    int findK2(std::vector<std::pair<char, int>> k2, char b) {
+    int findK2(vector<pair<char, int>> k2, char b) {
         for (auto i : k2) {
             if (i.first == b) {
                 return i.second;
             }
         }
-        return 0;
     }
 
     /*
@@ -132,47 +132,47 @@ public:
         //(1)
         while (depthSearch()) {
             if (choise) {
-                std::cout << "_______________________________________________\n\t\t\tFulkerson\n";
+                cout << "_______________________________________________\n\t\t\tFulkerson\n";
             }
             int flowInEdge = INT32_MAX;
 
             //(1.1)
             if (choise) {
-                std::cout << "Minimum bandwidth for path: " << runoff;
+                cout << "Minimum bandwidth for path: " << runoff;
             }
             for (tempRunoff = this->runoff; tempRunoff != this->source; tempRunoff = tempSource) {
                 tempSource = path[tempRunoff];
-                flowInEdge = std::min(flowInEdge, graphK1[tempSource][tempRunoff]);
+                flowInEdge = min(flowInEdge, graphK1[tempSource][tempRunoff]);
 
                 if (choise) {
-                    std::cout << "<-" << path[tempRunoff];
+                    cout << "<-" << path[tempRunoff];
                 }
             }
 
             if (choise) {
-                std::cout << ", is equal: " << flowInEdge << "\n";
+                cout << ", is equal: " << flowInEdge << "\n";
             }
 
             //(1.1.1)
             if (choise) {
-                std::cout<< "We go all the path and change the values of K1 and K2.\n"
+                cout<< "We go all the path and change the values of K1 and K2.\n"
                        "For the throughput of the rib and for the throughput in the opposite direction, respectively\n";
             }
             for (tempRunoff = this->runoff; tempRunoff != this->source; tempRunoff = tempSource) {
                 tempSource = path[tempRunoff];
                 if (choise) {
-                    std::cout<<"\t      "<<graphK1[tempSource][tempRunoff]<<"/"<<graphK2[tempRunoff][tempSource]<<"\n";
-                    std::cout<<"\t   "<<tempSource<<" <-> "<< tempRunoff<<"\n";
-                    std::cout << "\tK1 = " << graphK1[tempSource][tempRunoff] << " - " << flowInEdge << "\n";
-                    std::cout << "\tK2 = " << graphK2[tempRunoff][tempSource] << " + " << flowInEdge << "\n";
+                    cout<<"\t      "<<graphK1[tempSource][tempRunoff]<<"/"<<graphK2[tempRunoff][tempSource]<<"\n";
+                    cout<<"\t   "<<tempSource<<" <-> "<< tempRunoff<<"\n";
+                    cout << "\tK1 = " << graphK1[tempSource][tempRunoff] << " - " << flowInEdge << "\n";
+                    cout << "\tK2 = " << graphK2[tempRunoff][tempSource] << " + " << flowInEdge << "\n";
                 }
                 graphK1[tempSource][tempRunoff] -= flowInEdge;
                 graphK2[tempRunoff][tempSource] += flowInEdge;
                 if (choise) {
 
-                    std::cout << "\tK1 {" << tempSource << " -> " << tempRunoff << " = " << graphK1[tempSource][tempRunoff]
+                    cout << "\tK1 {" << tempSource << " -> " << tempRunoff << " = " << graphK1[tempSource][tempRunoff]
                          << "}\n";
-                    std::cout << "\tK2 {" << tempSource << " <- " << tempRunoff << " = " << graphK2[tempRunoff][tempSource]
+                    cout << "\tK2 {" << tempSource << " <- " << tempRunoff << " = " << graphK2[tempRunoff][tempSource]
                          << "}\n\n";
 
                 }
@@ -182,18 +182,18 @@ public:
 
             //(1.1.2)
             if (choise) {
-                std::cout<<"Add the minimum bandwidth to the maximum flow: "<<maxFlow<<" + "<< flowInEdge<<"\n";
+                cout<<"Add the minimum bandwidth to the maximum flow: "<<maxFlow<<" + "<< flowInEdge<<"\n";
             }
 
                 maxFlow += flowInEdge;
 
         }
         //(2)
-        std::cout << maxFlow << "\n";
+        cout << maxFlow << "\n";
         for (auto &vertex : graphK1) {
             for (auto neighbor : graphK1[vertex.first]) {
                 auto temp = findNeighbor(neighbor.first, graphK2);
-                std::cout << vertex.first << " " << neighbor.first << " " << findK2(temp, vertex.first) << "\n";
+                cout << vertex.first << " " << neighbor.first << " " << findK2(temp, vertex.first) << "\n";
             }
         }
 
@@ -202,25 +202,25 @@ public:
 
 private:
     char source, runoff; //исток, сток
-    std::map<char, std::map<char, int>> graphK1; //исходный граф
-    std::map<char, std::map<char, int>> graphK2; // граф с инвертироваными ребрами и проходящий через ребро поток. с->f стало f->c
-    std::map<char, char> path;
+    map<char, map<char, int>> graphK1; //исходный граф
+    map<char, map<char, int>> graphK2; // граф с инвертироваными ребрами и проходящий через ребро поток. с->f стало f->c
+    map<char, char> path;
 };
 
 
 int main() {
-    std::cout << "enable Intermediate data? 1 - Yes 0 - No\n";
-    std::cin >> choise;
+    cout << "enable Intermediate data? 1 - Yes 0 - No\n";
+    cin >> choise;
 
     if (choise) {
-        std::cout << "_____________Input graph__________________\n";
+        cout << "_____________Input graph__________________\n";
     }
 
     Graph g;
     g.inputGraph();
 
     if (choise) {
-        std::cout << "\n____________Fulkerson___________________\n";
+        cout << "\n____________Fulkerson___________________\n";
     }
     g.fulkerson();
     system("pause>nul");
