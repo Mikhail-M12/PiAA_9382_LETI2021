0c308cdb433d231443744388dbca94c0cf829acc
dmitrikuz <54910797+dmitrikuz@users.noreply.github.com>
2021-05-07 04:37:22 +0300

    lab4: rename

--- Kuzmin/lab4/runtests.sh
+++ Kuzmin/lab4/runtests.sh
@@ -1,19 +1,19 @@
 echo Test 1 KMP
 cat tests/test1.txt
-./lab4 < tests/test1.txt
+./kmp < tests/test1.txt
 echo -----
 echo Test 2 Circular
 cat tests/test2.txt
-./lab4_2 < tests/test2.txt
+./circular < tests/test2.txt
 echo -----
 echo Test 3 KMP
 cat tests/test3.txt
-./lab4 < tests/test3.txt
+./kmp < tests/test3.txt
 echo ------
 echo Test 4 Circular
 cat tests/test4.txt
-./lab4_2 < tests/test4.txt
+./circular < tests/test4.txt
 echo -----
 echo Test 5 KMP
 cat tests/test5.txt
-./lab4 < tests/test5.txt
+./kmp < tests/test5.txt
