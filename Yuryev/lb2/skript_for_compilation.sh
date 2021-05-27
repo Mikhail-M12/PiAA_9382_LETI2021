e583f4bfa1e0fbfc14cdc8457201ddfba239a8de
sergeyyurev <55050158+sergeyyurev@users.noreply.github.com>
2021-05-27 17:26:50 +0300

    Add files via upload

---
+++ Yuryev/lb2/skript_for_compilation.sh
@@ -0,0 +1,3 @@
+g++ -o lab2_greedy ./src/Greedy_alg.cpp
+g++ -o lab2_deikstra ./src/Deikstra_alg.cpp
+g++ -o lab2_astar ./src/AStar_alg.cpp
---
+++ Yuryev/lb2/skript_for_testing.sh
@@ -0,0 +1,20 @@
+echo "Greedy algo:\n"
+./lab2_greedy < tests/test1.txt
+./lab2_greedy < tests/test2.txt
+./lab2_greedy < tests/test3.txt
+./lab2_greedy < tests/test4.txt
+./lab2_greedy < tests/test5.txt
+
+echo "\nDeikstra algo:\n"
+./lab2_deikstra < tests/test1.txt
+./lab2_deikstra < tests/test2.txt
+./lab2_deikstra < tests/test3.txt
+./lab2_deikstra < tests/test4.txt
+./lab2_deikstra < tests/test5.txt
+
+echo "\nAStar algo:\n"
+./lab2_astar < tests/test1.txt
+./lab2_astar < tests/test2.txt
+./lab2_astar < tests/test3.txt
+./lab2_astar < tests/test4.txt
+./lab2_astar < tests/test5.txt
