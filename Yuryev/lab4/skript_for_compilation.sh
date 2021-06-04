a3ff4d311c7bf0969d966e3db2fe1bade22a3f37
sergeyyurev <55050158+sergeyyurev@users.noreply.github.com>
2021-06-04 20:43:51 +0300

    Add files via upload

---
+++ Yuryev/lab4/skript_for_compilation.sh
@@ -0,0 +1,2 @@
+g++ -o lab4_kmp ./src/lab4_1.cpp
+g++ -o lab4_shift ./src/lab4_2.cpp
---
+++ Yuryev/lab4/skript_for_testing.sh
@@ -0,0 +1,13 @@
+echo "First task:\n"
+./lab4_kmp < tests/test1_1.txt
+./lab4_kmp < tests/test1_2.txt
+./lab4_kmp < tests/test1_3.txt
+./lab4_kmp < tests/test1_4.txt
+./lab4_kmp < tests/test1_5.txt
+
+echo "\nSecond Task:\n"
+./lab4_shift < tests/test2_1.txt
+./lab4_shift < tests/test2_2.txt
+./lab4_shift < tests/test2_3.txt
+./lab4_shift < tests/test2_4.txt
+./lab4_shift < tests/test2_5.txt
